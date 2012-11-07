7d42f7d59a2700891da9b3fd44cdd2f519c52bf1
Michael Deng <michael.deng@duke.edu>
2012-11-07 16:45:11 -0500

    [arcade] Created IArcadeGame and GameManager as part of Arcade API

---
+++ src/arcade/GameManager.java
@@ -0,0 +1,32 @@
+package arcade;
+
+/**
+ * Class of the arcade used to start, end, and maintain 
+ * the relationship with all games in the arcade. 
+ * 
+ * @author Michael Deng
+ *
+ */
+public class GameManager {
+
+    /**
+     * Called by the game to store the user preferences, overwriting any existing preferences.
+     * @param userPreferences preferences 
+     */
+    public void saveUserPreferences(String userPreferences){
+        // TODO
+        
+    }
+
+    /**
+     * Adds the given score to the sorted list of scores for that user.
+     * @param score score to be saved
+     */
+    public void saveUserScore(int score){
+        // TODO
+        
+    }
+
+    
+    
+}
