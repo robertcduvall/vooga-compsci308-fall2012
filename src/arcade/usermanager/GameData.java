40489e61d975e75e527857d99cdbf7085928dc32
Howard Chung <howard.chung@duke.edu>
2012-11-08 03:58:05 -0500

    [arcade] framework for user management

---
+++ src/arcade/usermanager/GameData.java
@@ -0,0 +1,9 @@
+
+public class GameData {
+public String gameName, gameInfo;
+public int highScore, timesPlayed;
+
+public GameData(){
+    
+}
+}
---
+++ src/arcade/usermanager/User.java
@@ -0,0 +1,11 @@
+
+public class User {
+    public String name, picture;
+    int credits;
+    List<Message> messages;
+    List<GameData> gameData;
+
+    public User() {
+      
+    }
+}
