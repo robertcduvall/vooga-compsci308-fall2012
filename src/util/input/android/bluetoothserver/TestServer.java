01c509e384bc358c7efd9bdb21f9a5d4c59d4280
= <benjamin.schwab@duke.edu>
2012-11-09 16:04:59 -0500

    Input team initial API and external library jars for Wii and Android

---
+++ src/util/input/android/bluetoothserver/TestServer.java
@@ -0,0 +1,10 @@
+package util.input.android.bluetoothserver;
+
+public class TestServer {
+    
+    public static void main (String[] args){
+        AndroidBluetoothServer server = new AndroidBluetoothServer(0);
+        server.startServer();
+    }
+
+}
---
+++ src/util/input/android/events/AndroidMotionEvent.java
@@ -0,0 +1,11 @@
+package util.input.android.events;
+
+/**
+ * Waiting to research android hardware ability before commiting on the info we can provide
+ * @author Ben
+ *
+ */
+public class AndroidMotionEvent {
+    
+
+}
