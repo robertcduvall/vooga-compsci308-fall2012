af2164425518dbdffb32ca7ece00ffd2673b20a0
google.hsbzyq <google.hsbzyq@gmail.com>
2012-11-09 08:39:14 -0500

    [platfoormer] menu

---
+++ src/vooga/platformer/utility/GameListener.java
@@ -0,0 +1,13 @@
+package src.vooga.platformer.utility;
+
+/**
+ * Implement this interface to wrap the action invoked by pressing a button.
+ * @author Yaqi Zhang
+ *
+ */
+public interface GameListener {
+    /**
+     * 
+     */
+    void actionPerformed();
+}
