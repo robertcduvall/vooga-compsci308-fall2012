c69f09bd69488d1f0b8c18e50d134731e72c0132
Grant Oakley <geo4@gmail.com>
2012-11-09 00:37:24 -0500

    Added more get methods to Sprite.

--- src/vooga/platformer/leveleditor/Sprite.java
+++ src/vooga/platformer/leveleditor/Sprite.java
@@ -1,25 +1,34 @@
 package vooga.platformer.leveleditor;
 
 public class Sprite {
-    
+
+    public String getTag () {
+        // TODO
+        return "";
+    }
+
     public int getX () {
         // TODO
         return 0;
     }
-    
+
     public int getY () {
         // TODO
         return 0;
     }
-    
+
     public int getWidth () {
         // TODO
         return 0;
     }
-    
+
     public int getHeight () {
         // TODO
         return 0;
     }
 
+    public String getImagePath () {
+        // TODO
+        return "";
+    }
 }
