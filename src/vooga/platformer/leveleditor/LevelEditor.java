1220a47d1eb9637eba396ece91003e893c3ee55f
Sam Rang <srang2010@gmail.com>
2012-11-09 14:36:41 -0500

    adding methods to leveleditor

--- src/vooga/platformer/leveleditor/LevelEditor.java
+++ src/vooga/platformer/leveleditor/LevelEditor.java
@@ -1,6 +1,8 @@
 package vooga.platformer.leveleditor;
 
 import java.awt.Dimension;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 import javax.swing.JFrame;
 import javax.swing.UIManager;
 import javax.swing.UnsupportedLookAndFeelException;
@@ -28,8 +30,41 @@ public class LevelEditor extends JFrame{
         catch (UnsupportedLookAndFeelException e) {
             e.printStackTrace();
         }
+        createListeners();
+        createEditPane();
+        createButtonPanel();
         setVisible(true);
         pack();
+    }
+    private void createListeners () {
+//        addKeyListener(new KeyAdapter() {
+//            @Override
+//            public void keyPressed (KeyEvent e) {
+//                if (e.getKeyCode() == KeyEvent.VK_M) {
+//                    final Menu menu = new Menu(myPanel);
+//                    GameButton gb1 = new GameButton("greenbutton", "Back");
+//                    GameListener gl = new GameListener() {
+//                        @Override
+//                        public void actionPerformed () {
+//                            myPanel.remove(menu);
+//                            myFrame.repaint();
+//                        }
+//                    };
+//                    gb1.setGameListener(gl);
+//                    GameButton gb2 = new GameButton("button", "Do nothing");
+//                    gb2.setSize(new Dimension(130, 130));
+//                    menu.addButtons(gb1);
+//                    menu.addButtons(gb2);
+//                    myFrame.pack();
+//                }
+//            }
+//        });
+    }
+    private void createButtonPanel () {
+        
+    }
+    private void createEditPane () {
+        
     }
 
 }
