509f8af239893d6e2c95f331fdabf5ba5f00e083
mgovea <mgovea@radius>
2012-11-09 19:10:00 -0500

    improved Camera utilites' documentation

--- src/util/camera/SizedCamera.java
+++ src/util/camera/SizedCamera.java
@@ -1,32 +1,51 @@
 package util.camera;
 
+import java.awt.Rectangle;
 import java.awt.geom.Dimension2D;
 import java.awt.geom.Rectangle2D;
 
+
 /**
- * The <code>SizedCamera</code> abstract class is used to keeps track of a window
- * of a specific size.
- * large region.</br>
- * For instance, in a platformer, a small window of the level is actually
- * painted, and implementations of <code>Camera</code> can update where that
- * window actually is.
+ * The <code>SizedCamera</code> abstract class is used to keeps track of a
+ * rectangular
+ * window of a specific size within a larger rectangular region.
+ * 
  * @author Mark Govea
  */
 public abstract class SizedCamera implements Camera {
-    Rectangle2D myBounds;
-    Rectangle2D myOuterBounds;
-    Dimension2D mySize;
-    
+    private Rectangle2D myBounds;
+    private Rectangle2D myOuterBounds;
+    private Dimension2D mySize;
+
     public SizedCamera (Dimension2D cameraSize, Rectangle2D outerBounds) {
         mySize = cameraSize;
         myOuterBounds = outerBounds;
+        if (myBounds == null) {
+            myBounds = new Rectangle();
+        }
     }
 
     @Override
-    public abstract void update (Object ... args);
+    public abstract void update ();
 
     @Override
     public Rectangle2D getBounds () {
         return myBounds;
+    }
+
+    /**
+     * Returns the outer bounding box which this camera will stay within.
+     * @return The <code>SizedCamera</code>'s outer bounds.
+     */
+    protected Rectangle2D outerBounds () {
+        return myOuterBounds;
+    }
+
+    /**
+     * Returns the size of the camera's window.
+     * @return The <code>SizedCamera</code>'s size.
+     */
+    protected Dimension2D size () {
+        return mySize;
     }
 }
