1bea1bff435be0a640678150ea61a8dc2139745f
Niel Lebeck <niel.lebeck@gmail.com>
2012-11-11 13:16:36 -0500

    wrote level class

--- src/vooga/platformer/core/Level.java
+++ src/vooga/platformer/core/Level.java
@@ -2,25 +2,70 @@ package vooga.platformer.core;
 
 import util.camera.Camera;
 import vooga.platformer.gameobject.GameObject;
+import vooga.platformer.util.enums.PlayState;
 import java.awt.Graphics;
 import java.util.List;
 
+/**
+ * 
+ * @author Niel
+ *
+ */
+
 public abstract class Level {
     private List<GameObject> objectList;
+    private Camera cam;
     
+    /**
+     * Paint the level, including all its GameObjects.
+     * @param pen Graphics object to paint on
+     */
     public void paint(Graphics pen) {
+        paintBackground(pen);
         for (GameObject go : objectList) {
-            go.paint(pen, getCamera());
+            go.paint(pen, cam);
         }
     }
     
+    /**
+     * Template method. Paint the background of the level.
+     * @param pen Graphics object
+     */
+    public abstract void paintBackground(Graphics pen);
+    
+    /**
+     * Update the level. 
+     * @param elapsedTime time since last update cycle
+     */
     public void update(long elapsedTime) {
         for (GameObject go : objectList) {
             go.update(this, elapsedTime);
         }
     }
     
-    public abstract Camera getCamera();
+    /**
+     * @return the current camera of this level
+     */
+    public Camera getCamera() {
+        return cam;
+    }
     
-    public abstract String getLevelStatus();
+    /**
+     * Set the camera of this level.
+     * @param c
+     */
+    public void setCamera(Camera c) {
+        cam = c;
+    }
+    
+    /**
+     * @return a PlayState representing the progress of the player
+     * through the level.
+     */
+    public abstract PlayState getLevelStatus();
+    
+    /**
+     * @return a string representing the name of the next level to load
+     */
+    public abstract String getNextLevelName();
 }
---
+++ src/vooga/platformer/util/enums/PlayState.java
@@ -0,0 +1,5 @@
+package vooga.platformer.util.enums;
+
+public enum PlayState {
+    IS_PLAYING, NEXT_LEVEL, GAME_OVER;
+}
