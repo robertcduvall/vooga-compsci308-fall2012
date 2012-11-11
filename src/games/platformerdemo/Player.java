fe28ca1d72dd03279b1ccde2b0bc915e3e055563
google.hsbzyq <google.hsbzyq@gmail.com>
2012-11-11 16:53:43 -0500

    [platformer] playerMoveStrategy

--- src/games/platformerdemo/Player.java
+++ src/games/platformerdemo/Player.java
@@ -1,7 +1,26 @@
 package games.platformerdemo;
 
+import java.awt.Graphics;
+import java.awt.Image;
+import util.camera.Camera;
 import vooga.platformer.gameobject.GameObject;
 
 public class Player extends GameObject {
 
+
+    public Player (String configString) {
+        super(configString);
+    }
+
+    @Override
+    public void paint (Graphics pen, Camera cam) {
+        
+    }
+
+    @Override
+    public Image getCurrentImage () {
+        // TODO Auto-generated method stub
+        return null;
+    }
+ 
 }
