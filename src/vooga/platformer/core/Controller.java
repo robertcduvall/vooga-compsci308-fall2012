9d985753d3cce054f4002ffc5ca18c56b6e97b70
Niel Lebeck <niel.lebeck@gmail.com>
2012-11-11 13:35:39 -0500

    wrote controller skeleton

--- src/vooga/platformer/core/Controller.java
+++ src/vooga/platformer/core/Controller.java
@@ -1,9 +1,53 @@
 package vooga.platformer.core;
 
-public class Controller {
+import java.awt.Graphics;
+import javax.swing.JPanel;
+
+public class Controller extends JPanel implements Runnable {
+    private final int SLEEP_DELAY = 25;
+    
     private Level myCurrentLevel;
     
-    public void update() {
-        myCurrentLevel.update();
+    /**
+     * The main update cycle method.
+     * @param elapsedTime
+     */
+    public void update(long elapsedTime) {
+        myCurrentLevel.update(elapsedTime);
     }
+    
+    public void paint(Graphics pen) {
+        myCurrentLevel.paint(pen);
+    }
+    
+    /**
+     * The main game loop
+     */
+    public void run() {
+        long beforeTime, timeDiff, sleepTime;
+        
+        beforeTime = System.currentTimeMillis();
+        
+        while (true) {
+            timeDiff = System.currentTimeMillis() - beforeTime;
+
+                update(timeDiff);
+                repaint();
+                
+                timeDiff = System.currentTimeMillis() - beforeTime;
+                sleepTime = SLEEP_DELAY - timeDiff;
+                
+                if (sleepTime < 0) {
+                        sleepTime = 2;
+                }
+                
+                try {
+                        Thread.sleep(sleepTime);
+                }
+                catch(InterruptedException e) {
+                        System.out.println("interrupted!");
+                }
+                beforeTime = System.currentTimeMillis();
+        }
+}
 }
