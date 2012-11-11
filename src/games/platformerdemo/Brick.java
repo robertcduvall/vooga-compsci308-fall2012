9202298e2302d133f352047a665495e9f417d615
google.hsbzyq <google.hsbzyq@gmail.com>
2012-11-11 15:45:05 -0500

    [platformer] add brick

---
+++ src/games/platformerdemo/Brick.java
@@ -0,0 +1,28 @@
+package games.platformerdemo;
+
+import java.awt.Graphics;
+import java.awt.Image;
+import java.io.File;
+import java.io.IOException;
+import javax.imageio.ImageIO;
+import util.camera.Camera;
+import vooga.platformer.gameobject.GameObject;
+
+public class Brick extends GameObject{
+    private Image myImg;
+    public Brick (String configString){
+        super(configString);
+        try {
+            myImg = ImageIO.read(new File("src/games/platformerdemo/brick.png"));
+        }
+        catch (IOException e) {
+            System.out.println("no file find");
+            e.printStackTrace();
+        }
+    }
+    @Override
+    public void paint (Graphics pen, Camera cam) {
+        // TODO Auto-generated method stub
+        
+    }
+}
