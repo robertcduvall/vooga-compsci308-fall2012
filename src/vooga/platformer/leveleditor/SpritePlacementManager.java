81be8da8d975662c99a65db6c17d251485753ccd
paul.dannenberg <google.paul.dannenberg@gmail.com>
2012-11-11 21:37:36 -0500

    added some listeners for mouse selection

--- src/vooga/platformer/leveleditor/SpritePlacementManager.java
+++ src/vooga/platformer/leveleditor/SpritePlacementManager.java
@@ -1,6 +1,8 @@
 package vooga.platformer.leveleditor;
 
 import java.awt.Rectangle;
+import java.awt.Shape;
+import java.awt.geom.Point2D;
 import java.util.ArrayList;
 import java.util.Collection;
 
@@ -27,13 +29,13 @@ public class SpritePlacementManager implements ISpritePlacementManager {
      * @param board The board whose sprite placement this
      *        object will manage.
      */
-    public SpritePlacementManager(LevelBoard board) {
+    public SpritePlacementManager (LevelBoard board) {
         myLevelBoard = board;
         mySelectedSprites = new ArrayList<Sprite>();
     }
 
     @Override
-    public void positionSprite(Sprite sprite) {
+    public void positionSprite (Sprite sprite) {
         if (isValidPosition(sprite)) {
             myLevelBoard.add(sprite);
         }
@@ -41,26 +43,45 @@ public class SpritePlacementManager implements ISpritePlacementManager {
     }
 
     @Override
-    public void selectSprite(Sprite sprite) {
-        // TODO Auto-generated method stub
+    public void clearSelection () {
+        mySelectedSprites.clear();
+    }
+
+    @Override
+    public void selectSprite (Sprite sprite) {
+        mySelectedSprites.add(sprite);
 
     }
 
     @Override
-    public void clearSprite(Sprite toClear) {
-        myLevelBoard.remove(toClear);
+    public boolean selectSprite (Point2D point) {
+        int initialNumberSelected = mySelectedSprites.size();
+        selectRegion(new Rectangle((int) point.getX(), (int) point.getY(), 0, 0));
+        int finalNumberSelected = mySelectedSprites.size();
+        return finalNumberSelected != initialNumberSelected;
     }
 
     @Override
-    public void selectRegion(Rectangle region) {
-        // TODO Auto-generated method stub
+    public void removeSprite (Sprite toRemove) {
+        myLevelBoard.remove(toRemove);
+    }
+
+    @Override
+    public void selectRegion (Rectangle region) {
+        for (Sprite currentlyPlacedSprites : myLevelBoard.getSprites()) {
+            if (currentlyPlacedSprites.isIntersecting(region)) {
+                mySelectedSprites.add(currentlyPlacedSprites);
+            }
+        }
 
     }
 
     @Override
-    public boolean isValidPosition(Sprite sprite) {
-        return false;
-        // TODO Auto-generated method stub
+    public boolean isValidPosition (Sprite sprite) {
+        for (Sprite currentlyPlacedSprites : myLevelBoard.getSprites()) {
+            if (sprite.isIntersecting(currentlyPlacedSprites.getOutline())) { return false; }
+        }
+        return true;
 
     }
 
