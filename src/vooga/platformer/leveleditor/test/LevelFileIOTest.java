183f5b59b20a9e6bd4dc0cda26af42e146b79340
Grant Oakley <geo4@gmail.com>
2012-11-13 00:46:13 -0500

    Updated test file for FileIO.

--- src/vooga/platformer/leveleditor/test/LevelFileIOTest.java
+++ src/vooga/platformer/leveleditor/test/LevelFileIOTest.java
@@ -22,14 +22,13 @@ public class LevelFileIOTest {
     private static final String LEVEL_ID = "Level Name";
     private static final int LEVEL_WIDTH = 20;
     private static final int LEVEL_HEIGHT = 20;
-    private static final String LEVEL_IMAGE = "level.jpg";
+    private static final String TEST_IMAGE = "src/vooga/platformer/leveleditor/test/testImage.jpg";
 
     private static final String TYPE = "enemy";
     private static final int X_POS = 1;
     private static final int Y_POS = 1;
     private static final int SPRITE_WIDTH = 1;
     private static final int SPRITE_HEIGHT = 1;
-    private static final String SPRITE_IMAGE = "something.jpg";
 
     private static final String STRATEGY_TYPE_TAG = "type";
     private static final String STRATEGY_TYPE_VALUE = "movement";
@@ -49,11 +48,11 @@ public class LevelFileIOTest {
         strategy.put(STRATEGY_TYPE_TAG, STRATEGY_TYPE_VALUE);
         strategy.put(STRATEGY_PARAM_TAG, STRATEGY_PARAM_VALUE);
 
-        sprite = new Sprite(TYPE, X_POS, Y_POS, SPRITE_WIDTH, SPRITE_HEIGHT, SPRITE_IMAGE);
+        sprite = new Sprite(TYPE, X_POS, Y_POS, SPRITE_WIDTH, SPRITE_HEIGHT, TEST_IMAGE);
         sprite.addUpdateStrategy(strategy);
         sprite.addAttribute(SPRITE_ATTR_TAG, SPRITE_ATTR_VALUE);
         sprites.add(sprite);
-        LevelFileWriter.writeLevel(XML_FILE_PATH, LEVEL_ID, LEVEL_WIDTH, LEVEL_HEIGHT, LEVEL_IMAGE,
+        LevelFileWriter.writeLevel(XML_FILE_PATH, LEVEL_ID, LEVEL_WIDTH, LEVEL_HEIGHT, TEST_IMAGE,
                                    sprites);
         lfr = new LevelFileReader(XML_FILE_PATH);
         Assert.assertNotNull(lfr);
@@ -111,7 +110,7 @@ public class LevelFileIOTest {
 
     @Test
     public void testGetSpriteImage () throws Exception {
-        Assert.assertEquals(SPRITE_IMAGE, getFirstSprite().getImagePath());
+        Assert.assertEquals(TEST_IMAGE, getFirstSprite().getImagePath());
     }
 
     @Test
