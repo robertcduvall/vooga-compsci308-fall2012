024a1274e3d3730d9c4f500c6bcf879eb65d676f
Michael Deng <michael.deng@duke.edu>
2012-11-13 13:13:10 -0500

    [arcade] created datatransfer pkg; created onbutton class - used to start the arcade

---
+++ src/OnButton.java
@@ -0,0 +1,22 @@
+
+/**
+ * This class turns on the arcade. Just press the On Button. 
+ * 
+ * @author Michael Deng
+ *
+ */
+public class OnButton {
+
+    /**
+     * @param args
+     */
+    public static void main (String[] args) {
+
+        // creates JFrame and waits for button press.
+        
+        // Creates Arcade when button is pressed.
+        
+        
+    }
+
+}
