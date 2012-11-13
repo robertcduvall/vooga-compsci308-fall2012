840aa9c9c0bc9f2e831d5f50b5e17bf7b5483421
Michael Deng <michael.deng@duke.edu>
2012-11-13 14:03:09 -0500

    [vooga] finished implementing OnButton... now you can start the arcade with an On button

--- src/OnButton.java
+++ src/OnButton.java
@@ -1,11 +1,31 @@
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import arcade.gui.Arcade;
+import arcade.utility.ImageReader;
+
 
 /**
- * This class turns on the arcade. Just press the On Button. 
+ * This class turns on the arcade. Just press the On Button.
  * 
  * @author Michael Deng
- *
+ * 
  */
 public class OnButton {
+
+    private static JFrame startFrame;
+    private static JPanel startPanel;
+
+    private static final int frameHeight = 400;
+    private static final int frameWidth = 400;
+
+    private static final int buttonHeight = 400;
+    private static final int buttonWidth = 400;
 
     /**
      * @param args
@@ -13,10 +33,36 @@ public class OnButton {
     public static void main (String[] args) {
 
         // creates JFrame and waits for button press.
-        
-        // Creates Arcade when button is pressed.
-        
-        
+        startFrame = new JFrame("Are you ready?");
+        startFrame.setSize(new Dimension(frameWidth, frameHeight));
+        startPanel = new JPanel();
+        startPanel.setLayout(new BorderLayout());
+
+        addPowerButton();
+
+        startFrame.getContentPane().add(startPanel, BorderLayout.CENTER);
+
+        startFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        startFrame.pack();
+        startFrame.setVisible(true);
+
     }
 
+    private static void addPowerButton () {
+        ImageIcon icon =
+                new ImageIcon(ImageReader.loadImage("src/arcade/gui/images", "powerbutton.png"));
+        JButton powerButton = new JButton(icon);
+        powerButton.setPreferredSize(new Dimension(buttonWidth, buttonHeight));
+        powerButton.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed (ActionEvent e) {
+                @SuppressWarnings("unused")
+                Arcade theArcade = new Arcade();
+                startFrame.dispose();
+            }
+        });
+
+        startPanel.add(powerButton, BorderLayout.CENTER);
+    }
 }
---
+++ src/arcade/gui/Arcade.java
@@ -0,0 +1,17 @@
+package arcade.gui;
+
+/**
+ * 
+ * This is the top-level container manager for the arcade. It 
+ * builds and maintains the top-level JFrame for the arcade.
+ * 
+ * 
+ * @author Michael Deng
+ *
+ */
+public class Arcade {
+
+    public Arcade(){
+        System.out.println("got it!");
+    }
+}
