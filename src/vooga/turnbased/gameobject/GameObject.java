fbc1c09a71e16a2c5314421845fdc7833b30035f
Michael Elgart <mje15@duke.edu>
2012-11-12 20:21:43 -0500

    added javadoc comment and explained why GameObject.java is empty

--- src/vooga/turnbased/gameobject/GameObject.java
+++ src/vooga/turnbased/gameobject/GameObject.java
@@ -1,5 +1,15 @@
 package vooga.turnbased.gameobject;
 
+/**
+ * This class is a bucket. It has no functionality. We are looking into possible ways
+ * around this class having no functionality, but because MapObject and BattleObject
+ * are also abstract, any actual objects in the game could not inherit anything from
+ * GameObject, as that would be multiple inheritance. Thus, there is no point to add 
+ * functionality to this class as multiple inheritance assures that functionality 
+ * would never be used.
+ * @author Michael Elgart
+ *
+ */
 public abstract class GameObject {
 
 }
