9e26c3976d3b9d5b5a4017b76d9c15c6a13945df
Michael Deng <michael.deng@duke.edu>
2012-11-14 13:02:07 -0500

    [arcade] added constructors to main panel subclasses

--- src/arcade/gui/panel/main/ComposeMessageMainPanel.java
+++ src/arcade/gui/panel/main/ComposeMessageMainPanel.java
@@ -1,8 +1,14 @@
 package arcade.gui.panel.main;
 
+import arcade.gui.Arcade;
 import arcade.gui.panel.ArcadePanel;
 
 public class ComposeMessageMainPanel extends AMainPanel {
+
+    public ComposeMessageMainPanel (Arcade a) {
+        super(a);
+        // TODO Auto-generated constructor stub
+    }
 
     @Override
     public ArcadePanel createPanel () {
