daf5909f202ce7c1550d76ef6cd68ce1551f2241
Grant Oakley <geo4@gmail.com>
2012-11-14 00:54:04 -0500

    Began implementation of LevelFactory.

--- src/vooga/platformer/level/LevelFactory.java
+++ src/vooga/platformer/level/LevelFactory.java
@@ -1,7 +1,41 @@
 package vooga.platformer.level;
 
-public abstract class LevelFactory {
+import vooga.platformer.gameobject.GameObject;
+import vooga.platformer.leveleditor.LevelFileReader;
+import vooga.platformer.leveleditor.Sprite;
 
-    public abstract Level loadLevel(String levelName);
+
+/**
+ * A single-method class used to construct Level objects from formatted data
+ * files.
+ * 
+ * @author Grant Oakley
+ * 
+ */
+public final class LevelFactory {
+
+    private LevelFactory () {
+        /*
+         * Empty constructor.
+         */
+    }
+
+    /**
+     * Creates a new Level from the file at the path specified. The format of
+     * this file must conform to the conventions of LevelFileReader.
+     * 
+     * @param levelName path to the data file describing the level
+     * @return a Level object with all the properties and GameObjects specified
+     *         in the data file
+     */
+    public static Level loadLevel (String levelName) {
+        LevelFileReader lfr = new LevelFileReader(levelName);
+        return null;
+    }
+
+    private static GameObject spriteToGameObject (Sprite s) {
+        // TODO
+        return null;
+    }
 
 }
