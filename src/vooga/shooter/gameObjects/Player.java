3d54b761ce67e6608827b46922092d319fac1f40
Jesse Starr <jesse.starr@duke.edu>
2012-11-13 23:28:17 -0500

    added some of the movement methods for player.java in shooter game

--- src/vooga/shooter/gameObjects/Player.java
+++ src/vooga/shooter/gameObjects/Player.java
@@ -26,4 +26,58 @@ public class Player extends Sprite{
     public void continueUpdate () {
         
     }
+
+    /**
+     * Changes the player's velocity to add a -x component,
+     * while keeping the current y velocity.
+     * (This makes the player's sprite go left on the screen).
+     */
+    public void goLeft() {
+        Point v = new Point(-10, getVelocity().y);
+        setVelocity(v);
+        update();
+    }
+
+    /**
+     * Changes the player's velocity to add a +x component,
+     * while keeping the current y velocity.
+     * (This makes the player's sprite go right on the screen).
+     */
+    public void goRight() {
+        Point v = new Point(10, getVelocity().y);
+        setVelocity(v);
+        update();
+    }
+
+    /**
+     * Changes the player's velocity to add a -y component,
+     * while keeping the current x velocity.
+     * (This makes the player's sprite go up on the screen).
+     */
+    public void goUp() {
+        Point v = new Point(getVelocity().x, -10);
+        setVelocity(v);
+        update();
+    }
+
+    /**
+     * Changes the player's velocity to add a +y component,
+     * while keeping the current x velocity.
+     * (This makes the player's sprite go down on the screen).
+     */
+    public void goDown() {
+        Point v = new Point(getVelocity().x, 10);
+        setVelocity(v);
+        update();
+    }
+
+    /**
+     * Called if none of the keys that move the player are
+     * pressed. (Makes the player sprite stop moving).
+     */
+    public void stopMove() {
+        Point v = new Point(0, 0);
+        setVelocity(v);
+        update();
+    }
 }
