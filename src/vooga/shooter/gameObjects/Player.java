b6e63ba23a9c128f132ffea40a80f7c2ad804df7
Jesse Starr <jesse.starr@duke.edu>
2012-11-13 20:21:34 -0500

    starting to fix all the sprite classes (basic implementation for sprite.java and player.java), more to come

--- src/vooga/shooter/gameObjects/Player.java
+++ src/vooga/shooter/gameObjects/Player.java
@@ -1,70 +1,45 @@
 package vooga.shooter.gameObjects;
 
+import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Image;
 import java.awt.Point;
 import java.util.List;
 
 public class Player extends Sprite{
+    private int myHealth;
 
+    /**
+     * Constructs a player for the user to control.
+     * @param position the center of the sprite image
+     * @param size the size of the sprite image
+     * @param image the image to use
+     * @param health the starting health
+     */
+    public Player (Point position, Dimension size, Image image, int health) {
+        super(position, size, image);
+        setHealth(health);
+    }
+
+    /**
+     * This method is called after the player's position
+     * is updated and will include anything extra that the player
+     * needs to do when being updated (e.g. stop when at a wall).
+     */
     @Override
-    public void draw (Graphics g) {
-        // TODO Auto-generated method stub
+    public void continueUpdate () {
         
     }
 
-    @Override
-    public void update () {
-        // TODO Auto-generated method stub
-        
+    private void setHealth(int h) {
+        myHealth = h;
     }
 
-    @Override
-    public void delete () {
-        // TODO Auto-generated method stub
-        
+    /**
+     * Returns the health of the player.
+     * @return myHealth
+     */
+    public int getHealth() {
+        return myHealth;
     }
-
-    @Override
-    public Point getPosition () {
-        // TODO Auto-generated method stub
-        return null;
-    }
-
-    @Override
-    public void setPosition (Point position) {
-        // TODO Auto-generated method stub
-        
-    }
-
-    @Override
-    public Point getDirection () {
-        // TODO Auto-generated method stub
-        return null;
-    }
-
-    @Override
-    public void setDirection (Point direction) {
-        // TODO Auto-generated method stub
-        
-    }
-
-    @Override
-    public Image getImage () {
-        // TODO Auto-generated method stub
-        return null;
-    }
-
-    @Override
-    public void setImage (Image image) {
-        // TODO Auto-generated method stub
-        
-    }
-
-    @Override
-    public List<Sprite> collisions () {
-        // TODO Auto-generated method stub
-        return null;
-    }
-
 }
