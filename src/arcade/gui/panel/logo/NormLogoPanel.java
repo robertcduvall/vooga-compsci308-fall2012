b452b47d0894ace45688f5024d351f0b7f3ebf2b
Michael Deng <michael.deng@duke.edu>
2012-11-14 21:10:42 -0500

    [arcade] finished arcade logo, and improved onButton

--- src/arcade/gui/panel/logo/NormLogoPanel.java
+++ src/arcade/gui/panel/logo/NormLogoPanel.java
@@ -1,9 +1,16 @@
 package arcade.gui.panel.logo;
 
 import java.awt.Color;
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import edu.cmu.relativelayout.Binding;
+import edu.cmu.relativelayout.BindingFactory;
+import edu.cmu.relativelayout.RelativeConstraints;
+import edu.cmu.relativelayout.RelativeLayout;
 import arcade.gui.Arcade;
 import arcade.gui.panel.ArcadePanel;
+import arcade.utility.ImageReader;
 
 
 /**
@@ -15,14 +22,31 @@ public class NormLogoPanel extends ALogoPanel {
 
     public NormLogoPanel (Arcade a) {
         super(a);
-        // TODO Auto-generated constructor stub
+
     }
 
     @Override
     public ArcadePanel createPanel () {
         ArcadePanel myPanel = initializeNewPanel();
-        myPanel.setBackground(Color.GREEN);
         System.out.println("NormLogoPanel");
+
+        
+        myPanel.setBackground(Color.BLACK);
+        myPanel.setLayout(new RelativeLayout());
+        BindingFactory bf = new BindingFactory();
+        
+        Binding leftEdge = bf.leftEdge();
+        Binding topEdge = bf.topEdge();
+        Binding bottomEdge = bf.bottomEdge();
+        Binding rightEdge = bf.rightEdge();
+        RelativeConstraints imageConstraints = new RelativeConstraints();
+        imageConstraints.addBindings(leftEdge, topEdge, bottomEdge, rightEdge);
+              
+        ImageIcon icon =
+                new ImageIcon(ImageReader.loadImage("src/arcade/gui/images", "Arcade_logo.png"));
+        JLabel picLabel = new JLabel(icon);
+        
+        myPanel.add(picLabel, imageConstraints);
         
         return myPanel;
     }
