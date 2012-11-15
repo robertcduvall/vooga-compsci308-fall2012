ed9195a33f58c4c1476b82022dbda127dd6b7fc9
Robert Bruce <rwb392@gmail.com>
2012-11-14 22:55:57 -0500

    Changes to LoginMainPanel.java

--- src/arcade/gui/panel/main/LoginMainPanel.java
+++ src/arcade/gui/panel/main/LoginMainPanel.java
@@ -1,8 +1,17 @@
 package arcade.gui.panel.main;
 
+import java.awt.Color;
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
+import edu.cmu.relativelayout.Binding;
+import edu.cmu.relativelayout.BindingFactory;
+import edu.cmu.relativelayout.RelativeConstraints;
+import edu.cmu.relativelayout.RelativeLayout;
 import arcade.gui.Arcade;
 import arcade.gui.panel.ArcadePanel;
+import arcade.utility.ImageReader;
 
 /**
  * 
@@ -10,17 +19,33 @@ import arcade.gui.panel.ArcadePanel;
  * 
  */
 public class LoginMainPanel extends AMainPanel {
-
+    
     public LoginMainPanel (Arcade a) {
         super(a);
-
+        createPanel();
     }
 
     @Override
     public ArcadePanel createPanel () {
-        // TODO
+        ArcadePanel myPanel = initializeNewPanel();
+        myPanel.setBackground(Color.GRAY);
+        myPanel.setLayout(new RelativeLayout());
+        BindingFactory bf = new BindingFactory();
 
-        return null;
+        Binding leftEdge = bf.leftEdge();
+        Binding topEdge = bf.topEdge();
+        Binding bottomEdge = bf.bottomEdge();
+        Binding rightEdge = bf.rightEdge();
+        RelativeConstraints imageConstraints = new RelativeConstraints();
+        imageConstraints.addBindings(leftEdge, topEdge, bottomEdge, rightEdge);
+        JTextField textField = new JTextField(20);
+        ImageIcon icon = new ImageIcon(ImageReader.loadImage("src/arcade/gui/images", "Arcade_logo2.png"));
+        JLabel picLabel = new JLabel(icon);
+        
+        myPanel.add(textField, imageConstraints);
+
+        myPanel.add(picLabel, imageConstraints);
+        return myPanel;
     }
 
 }
