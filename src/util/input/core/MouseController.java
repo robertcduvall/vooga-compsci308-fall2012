53a7d4bf257d5c597455a4a17a9f5436ad5da06b
Mark Hoffman <mah75@duke.edu>
2012-11-14 22:14:15 -0500

    added util.xml package to branch for XmlParser

---
+++ src/util/input/core/MouseController.java
@@ -0,0 +1,106 @@
+package util.input.core;
+
+import java.awt.Component;
+import java.awt.event.MouseEvent;
+import javax.swing.event.MouseInputListener;
+import util.input.input_utils.UKeyCode;
+
+/**
+ * This class allows users to enter input through the mouse
+ * 
+ * @author Amay
+ */
+
+public class MouseController extends Controller<MouseInputListener> implements MouseInputListener {
+    
+    public final static int PRESSED = MouseEvent.MOUSE_PRESSED;
+    public final static int RELEASED = MouseEvent.MOUSE_RELEASED;
+    public final static int CLICKED = MouseEvent.MOUSE_CLICKED;
+    public final static int NO_BUTTON = 0;
+    
+    
+    /**
+     * Create a new mouse controller
+     * 
+     * @param comp - The component to which we add the MouseListener and MouseMotionListener 
+     */
+    public MouseController (Component comp) {
+        super();
+        comp.addMouseListener(this);
+        comp.addMouseMotionListener(this);
+    }
+    
+    @Override
+    public void mouseClicked (MouseEvent e) {
+        try {
+            performReflections(e, "mouseClicked", UKeyCode.codify(MouseEvent.MOUSE_CLICKED, e.getButton()));
+        }
+        catch (Exception e1) {
+            e1.printStackTrace();
+        }
+    }
+
+    @Override
+    public void mousePressed (MouseEvent e) {
+        try {
+            performReflections(e, "mousePressed", UKeyCode.codify(MouseEvent.MOUSE_PRESSED, e.getButton()));
+            System.out.println(e.getButton());
+        }
+        catch (Exception e1) {
+            e1.printStackTrace();
+        }
+    }
+
+    
+    @Override
+    public void mouseReleased (MouseEvent e) {
+        try {
+            performReflections(e, "mouseReleased", UKeyCode.codify(MouseEvent.MOUSE_RELEASED, e.getButton()));
+        }
+        catch (Exception e1) {
+            e1.printStackTrace();
+        }
+        
+    }
+
+    @Override
+    public void mouseEntered (MouseEvent e) {
+        try {
+            performReflections(e, "mouseEntered", UKeyCode.codify(MouseEvent.MOUSE_ENTERED, NO_BUTTON));
+        }
+        catch (Exception e1) {
+            e1.printStackTrace();
+        }       
+    }
+
+    @Override
+    public void mouseExited (MouseEvent e) {
+        try {
+            performReflections(e, "mouseExited", UKeyCode.codify(MouseEvent.MOUSE_EXITED, NO_BUTTON));
+        }
+        catch (Exception e1) {
+            e1.printStackTrace();
+        }
+    }
+
+    @Override
+    public void mouseDragged (MouseEvent e) {
+        try {
+            performReflections(e, "mouseDragged", UKeyCode.codify(MouseEvent.MOUSE_DRAGGED, e.getButton()));
+          }
+          catch (Exception e1) {
+              e1.printStackTrace();
+          }
+    }
+
+    @Override
+    public void mouseMoved (MouseEvent e) {
+        try {
+            performReflections(e, "mouseMoved", UKeyCode.codify(MouseEvent.MOUSE_MOVED, NO_BUTTON));
+          }
+          catch (Exception e1) {
+              e1.printStackTrace();
+          }
+    }
+
+}
---
+++ src/util/xml/XmlParser.java
@@ -0,0 +1,129 @@
+package util.xml;
+
+
+import java.awt.Image;
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import javax.imageio.ImageIO;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+
+/**
+ * Parses Xml to get data from the file.
+ * 
+ * @author Mark Hoffman
+ * 
+ */
+public class XmlParser {
+
+    private File myXmlFile;
+    private Document myXmlDocument;
+
+    /**
+     * Initiates the process for parsing an Xml file.
+     *
+     * @param file The Xml file that will be parsed.
+     */
+    public XmlParser (File file) {
+        myXmlFile = file;
+        makeDocument();
+    }
+
+    /**
+     * Enables parsing of the Xml file.  myXmlDocument is then the basis
+     * for the rest of the parsing.
+     */
+    private void makeDocument () {
+        DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+        try {
+            myXmlDocument = dbFactory.newDocumentBuilder().parse(myXmlFile);
+        }
+        catch (SAXException e) {
+            e.printStackTrace();
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+        catch (ParserConfigurationException e) {
+            e.printStackTrace();
+        }
+        myXmlDocument.normalize();
+    }
+
+    /**
+     * 
+     * @return Document (Root) Element
+     */
+    public Element getDocumentElement () {
+        return myXmlDocument.getDocumentElement();
+    }
+    
+    /**
+     * 
+     * @param element The tag that contains a section of XML to look through
+     * @param name The string name of the desired elements
+     * @return A node list containing the desired elements and their
+     *         associated section of XML
+     */
+    public NodeList getElementsByName (Element element, String name) {
+        NodeList list = element.getElementsByTagName(name);
+        return list;
+    }
+
+    /**
+     * 
+     * @param element The tag that contains a section of XML to look through
+     * @param tagName The string name of the element containing desired text
+     * @return The text contained in these desired tag
+     */
+    public String getTextContent (Element element, String tagName) {
+        NodeList list = element.getElementsByTagName(tagName);
+        return list.item(0).getTextContent();
+    }
+
+    /**
+     * 
+     * @param element The tag that contains a section of XML to look through
+     * @param tagName The string name of the element containing desired image
+     * @return The image in the desired tag
+     */
+    public Image getImageContent(Element element, String tagName) {
+        String pathName = getTextContent(element, tagName);
+        Image image = null;
+        try {
+            image = ImageIO.read(new File(pathName));
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+        return image;
+    }
+
+    /**
+     * 
+     * @param element The tag that contains a section of XML to look through
+     * @param tagName The string name of the element containing desired int
+     * @return The int value of the desired tag
+     */
+    public int getIntContent (Element element, String tagName) {
+        NodeList list = element.getElementsByTagName(tagName);
+        return Integer.parseInt(list.item(0).toString());
+    }
+
+    /**
+     * 
+     * @param element The tag that contains a section of XML to look through
+     * @param tagName The string name of the element containing desired double
+     * @return the double value of the desired tag
+     */
+    public double getDoubleContent (Element element, String tagName) {
+        NodeList list = element.getElementsByTagName(tagName);
+        return Double.parseDouble(list.item(0).toString());
+    }
+}
