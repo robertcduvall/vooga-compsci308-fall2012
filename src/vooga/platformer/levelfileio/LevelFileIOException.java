76ed5488f153cc2697769fce02f2ebd0c59a9264
Grant Oakley <geo4@gmail.com>
2012-11-15 00:38:49 -0500

    Added LevelFileIOException.

---
+++ src/vooga/platformer/levelfileio/LevelFileIOException.java
@@ -0,0 +1,20 @@
+package vooga.platformer.levelfileio;
+
+/**
+ * A general exception that represents all possible exceptions while reading and
+ * writing a platformer level data file.
+ * 
+ * @author Robert C. Duvall
+ * @author Grant Oakley (repurposed)
+ */
+@SuppressWarnings("serial")
+public class LevelFileIOException extends RuntimeException {
+    /**
+     * Creats a new LevelFileIOException with the specified error message.
+     * 
+     * @param s error message
+     */
+    public LevelFileIOException (String s) {
+        super(s);
+    }
+}
