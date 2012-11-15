af1f77f8ada52949d1286f83130c8c2bc880399c
Mark Hoffman <mah75@duke.edu>
2012-11-14 22:23:41 -0500

    Added a XmlTester file and removed duplicate XmlParser

---
+++ src/vooga/turnbased/gamecreation/XmlTester.java
@@ -0,0 +1,43 @@
+package vooga.turnbased.gamecreation;
+
+import java.awt.Image;
+import java.io.File;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+import util.xml.XmlParser;
+
+public class XmlTester {
+
+    /**
+     * @param args
+     */
+    public static void main (String[] args) {
+        String xmlPath = "src/vooga/turnbased/resources/Example.xml";
+        File xmlFile = new File(xmlPath);
+        XmlParser parser = new XmlParser(xmlFile);
+        
+        Element root = parser.getDocumentElement();
+        System.out.println(root.getNodeName().toString());
+        
+        NodeList dimensionList = parser.getElementsByName(root, "dimension");
+        Element dimension = (Element) dimensionList.item(0);
+        System.out.println("  " + dimension.getNodeName());
+        String width = parser.getTextContent(dimension, "width");
+        String height = parser.getTextContent(dimension, "height");
+        System.out.println("    width: " + width);
+        System.out.println("    height: " + height);
+        
+        Image background = parser.getImageContent(root, "backgroundImage");
+        System.out.println("  Image: " + background);
+        
+        NodeList eventList = parser.getElementsByName(root, "event");
+        System.out.println("  " + eventList.item(0).getNodeName());
+        for (int i = 0; i < eventList.getLength(); i++) {
+            Element event = (Element) eventList.item(i);
+            String eventName = parser.getTextContent(event, "name");
+            String eventDestination = parser.getTextContent(event, "destination");
+            System.out.println("    name: " + eventName);
+            System.out.println("    destination: " + eventDestination);
+        }
+    }
+}
