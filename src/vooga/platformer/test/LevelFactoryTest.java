d0102286fbb9fbbb00f49b1849c0adf2fa2b9175
Grant Oakley <geo4@gmail.com>
2012-11-15 21:08:34 -0500

    Initial implementation of LevelFactory using some defaults instead of data file values.

--- src/vooga/platformer/test/LevelFactoryTest.java
+++ src/vooga/platformer/test/LevelFactoryTest.java
@@ -1,8 +1,10 @@
 package vooga.platformer.test;
 
+import games.platformerdemo.Enemy;
 import junit.framework.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import util.reflection.Reflection;
 import vooga.platformer.level.LevelFactory;
 import vooga.platformer.leveleditor.Sprite;
 import vooga.platformer.levelfileio.LevelFileReader;
@@ -19,7 +21,13 @@ public class LevelFactoryTest {
     private static final String XML_FILE_PATH = "src/vooga/platformer/test/testLevelFactory.xml";
 
     @Test
+    public void testReflection () throws Exception {
+        Assert.assertNotNull(Reflection.createInstance("games.platformerdemo.Player",
+                                                       "x=1,y=1,width=1,height=1"));
+    }
+
+    @Test
     public void testLevelIsCreated () throws Exception {
-        Assert.assertNotNull(LevelFactory.loadLevel(XML_FILE_PATH));
+        Assert.assertNotNull(LevelFactory.loadLevel(XML_FILE_PATH, "games.platformerdemo.Player"));
     }
 }
