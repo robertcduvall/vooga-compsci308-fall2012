cc5009aad5497bf5828cdd202c120cf8b869b83a
Jei Min Yoo <yjm607@gmail.com>
2012-11-17 15:10:11 -0500

    [arcade] more work on GameCenter

---
+++ src/arcade/Sample1.java
@@ -0,0 +1,39 @@
+package arcade;
+
+import java.awt.Image;
+import java.util.List;
+import arcade.gamemanager.GameSaver;
+
+public class Sample1 implements IArcadeGame {
+
+    @Override
+    public void runGame (String userPreferences, GameSaver s) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public List<Image> getScreenshots () {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Image getMainImage () {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public String getDescription () {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public String getName () {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+}
--- src/arcade/gamemanager/GameCenter.java
+++ src/arcade/gamemanager/GameCenter.java
@@ -11,156 +11,89 @@ import arcade.IArcadeGame;
 import arcade.usermanager.SocialCenter;
 import arcade.utility.ReadWriter;
 
+
 /**
+ * This class keeps a list of Games and returns appropriate Games at GUI's request.
+ * @author Jei Min Yoo
  * 
- * @author Jei Min Yoo, Seon Kang
- *
  */
 public class GameCenter {
 
     private XmlParser myXmlParser;
-    private List<IArcadeGame> myGames;
-    private NodeList gameNodeList;
+    private List<Game> myGames;
     private SocialCenter socialCenter;
-    
-    public GameCenter() {
+
+    public GameCenter () {
         initialize();
     }
-    
+
     /**
      * initializes the class by reading information from game.xml file.
      */
-    public void initialize() {
-        socialCenter = SocialCenter.getInstance();
+    public void initialize () {
+//        socialCenter = SocialCenter.getInstance();
         File f = new File("../vooga-compsci308-fall2012/src/arcade/database/game.xml");
         myXmlParser = new XmlParser(f);
-        gameNodeList = myXmlParser.getElementsByName(myXmlParser.getDocumentElement(), "game");
+        myGames = new ArrayList<Game>();
+        refreshGames();
+
     }
 
-    /**
-     * General function for getting String lists by passing a string of
-     * the tag.
-     * 
-     * @author Seon Kang
-     * 
-     * @param tag 
-     * 
-     * @return A String ArrayList of elements matching tag
-     */
-    public List<String> getListByTagName(String tag, Element element) {
-    	List<String> stringListByTag = new ArrayList<String>();
-    	NodeList nodeList = myXmlParser.getElementsByName(element, tag);
-    	for (int i = 0; i < nodeList.getLength(); i++) {
-    		stringListByTag.add(nodeList.item(i).getTextContent());
-    	}
-    	return stringListByTag;
-    }
+    private void refreshGames () {
+        myGames.clear();
+        NodeList nList =
+                myXmlParser.getElementsByName(myXmlParser.getDocumentElement(), "filepath");
 
-    /**
-     * Default element is the document.
-     * 
-     * @auther Seon Kang
-     * 
-     * @param tag
-     * @return
-     */
-    public List<String> getListByTagName(String tag) {
-    	return getListByTagName(tag, myXmlParser.getDocumentElement());
-    }
-    
-    
-    public List<String> getListOfGames() {
-        List<String> gameList = new ArrayList<String>();
-        NodeList nList = myXmlParser.getElementsByName(myXmlParser.getDocumentElement(), "name");
         for (int i = 0; i < nList.getLength(); i++) {
-            gameList.add(nList.item(i).getTextContent());
+            String filePath = nList.item(i).getTextContent();
+            try {
+                IArcadeGame arcade = (IArcadeGame) Class.forName(filePath).newInstance();
+                Game game = new Game(arcade);
+                myGames.add(game);
+            }
+            catch (IllegalAccessException e) {
+                System.out.println("illegal access: " + filePath);
+            }
+            catch (InstantiationException e) {
+                System.out.println("failed to instantiate class: " + filePath);
+            }
+            catch (ClassNotFoundException e) {
+                System.out.println("class does not exist: " + filePath);
+            }
+        }
+    }
+    
+    public List<String> getListOfGames () {
+        List<String> gameList = new ArrayList<String>();
+        for (Game game : myGames) {
+            gameList.add(game.getGameName());
         }
         return gameList;
     }
-    
-    /**
-     * A more general way to get game properties.
-     * 
-     * @author Seon Kang
-     * @param gameName 
-     * @param tag 
-     * @return
-     */
-    
-    public String getAttributeFromGame(String gameName, String tag) {
-    	for (int i = 0; i < gameNodeList.getLength(); i++) {
-    		if (gameNodeList.item(i).getTextContent().equals(gameName)) {
-    			NodeList attributeList = gameNodeList.item(i).getChildNodes();
-    			for (int j = 0; j < attributeList.getLength(); j++) {
-    				return myXmlParser.getTextContent((Element) gameNodeList.item(j), 
-    						tag);
-    			}
-    		}
-    	}
-    	return null;
-    }
-    
-    public String getRating(String gameName) {
-        
-        for (int i = 0; i < gameNodeList.getLength(); i++) {
-            NodeList gameInfoList = gameNodeList.item(i).getChildNodes();
-            for (int j = 0; j < gameInfoList.getLength(); j++) {
-                if (gameInfoList.item(j).getNodeName().equals(gameName)) {
-                  return myXmlParser.getTextContent((Element) gameNodeList.item(i), "rating");  
-                }
-            }
-        }
-        
-        return "0";
-    }
-    
-    /**
-     * Find a list of attributes from gameName.
-     * 
-     * @auther Seon Kang
-     * 
-     * @param gameName
-     * @param tag 
-     * @return
-     */
-    public List<String> getListOfX(String gameName, String tag) {
-    	Element gameAsElement = myXmlParser.getDocumentElement();
-    	for (int i = 0; i < gameNodeList.getLength(); i++) {
-    		if (tag.equals(gameNodeList.item(i).getTextContent())) {
-    			gameAsElement = (Element) gameNodeList.item(i);
-    		}
-    	}
-    	return getListByTagName(tag, gameAsElement);
-    }
-    
-    public List<String> getListOfReviews(String gameName) {
-        List<String> reviewList = new ArrayList<String>();
-        
-        return reviewList;
-    }
-    
-    public List<String> getUsersThatPlayedGame(String gameName) {
-        List<String> userList = new ArrayList<String>();
-        return userList;
-    }
-    
-    public List<Integer> getUserScores(String gameName){
-        List<Integer> userList = new ArrayList<Integer>();
-        return userList;
-    }
-    
-    public Image getGameProfilePicture(String gameName) {
-        for (IArcadeGame game : myGames) {
-            if (game.getName().equals(gameName)) {
-                return game.getMainImage();
-            }
+
+    public Game getGame (String gameName) {
+        for (Game gm : myGames) {
+            if (gm.getGameName().equals(gameName)) { return gm; }
         }
         return null;
-        
     }
-    
-    public static void main(String args[]) {
-        System.out.println("haha");
-        GameCenter gc = new GameCenter();
+
+    public List<Game> getGameByTagName (String tag) {
+        List<Game> games = new ArrayList<Game>();
+        for (Game gm : myGames) {
+            if (gm.getGenre().contains(tag)) {
+                games.add(gm);
+            }
+        }
+        return games;
     }
+
+//     public static void main(String args[]) {
+//     System.out.println("haha");
+//     GameCenter gc = new GameCenter();
+//     List<String> list = gc.getListOfGames();
+//     gc.refreshGames();
+//     System.out.println(list);
+//     System.out.println(list.get(0).length());
+//     }
 }
