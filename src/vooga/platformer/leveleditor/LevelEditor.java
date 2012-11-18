dded83963e79fa2a0f7c521decd0a1761cfaa84a
Sam Rang <srang2010@gmail.com>
2012-11-18 16:12:58 -0500

    Adding more drop down menu functionality

--- src/vooga/platformer/leveleditor/LevelEditor.java
+++ src/vooga/platformer/leveleditor/LevelEditor.java
@@ -218,8 +218,8 @@ public class LevelEditor extends JFrame {
         try {
             docBuilder = docFactory.newDocumentBuilder();
             Document doc = docBuilder.newDocument();
-
-        } catch (ParserConfigurationException e) {
+        } 
+        catch (ParserConfigurationException e) {
             System.out.println("Directory does not exist, try again");
             newLevel();
         }
@@ -241,8 +241,10 @@ public class LevelEditor extends JFrame {
         int returnVal = chooser.showOpenDialog(chooser);
         if (returnVal == JFileChooser.APPROVE_OPTION) {
             try {
-                myBoard.load(new URL(chooser.getSelectedFile().getPath()));
-            } catch (IOException io) {
+                URL myURL = new URL(chooser.getSelectedFile().getPath());
+                myBoard.load(myURL);
+            } 
+            catch (IOException io) {
                 System.out.println("File not found. Try again");
             }
         }
