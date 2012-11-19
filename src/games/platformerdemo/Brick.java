c895e471addacf1ef9b01c2d7a9f2b7ef77d9011
Grant Oakley <geo4@gmail.com>
2012-11-19 00:54:47 -0500

    Refactored Brick to load its image from a config string rather than hardcoded values.

--- src/games/platformerdemo/Brick.java
+++ src/games/platformerdemo/Brick.java
@@ -3,26 +3,45 @@ package games.platformerdemo;
 import java.awt.Image;
 import java.io.File;
 import java.io.IOException;
+import java.util.Map;
 import javax.imageio.ImageIO;
 import vooga.platformer.gameobject.GameObject;
 
 
+/**
+ * 
+ * @author Grant Oakley (modified)
+ * 
+ */
 public class Brick extends GameObject {
     private Image myImg;
 
-    public Brick(String configString) {
+    public Brick (String configString) {
         super(configString);
+        Map<String, String> parsedConfigString = parseConfigString(configString);
+        String imagePath = parsedConfigString.get(DEFAULT_IMAGE_TAG);
+        if (imagePath == null) {
+            // TODO throw exception for poorly formatted config string
+        }
+
         try {
-            myImg = ImageIO
-                    .read(new File("src/games/platformerdemo/brick-small.png"));
-        } catch (IOException e) {
-            System.out.println("no file find");
+            myImg = ImageIO.read(new File(imagePath));
+        }
+        catch (IOException e) {
+            System.out.println("No file found.");
             e.printStackTrace();
         }
     }
 
     @Override
-    public Image getCurrentImage() {
+    public Image getCurrentImage () {
         return myImg;
+    }
+
+    @Override
+    public Map<String, String> getConfigStringParams () {
+        Map<String, String> params = super.getConfigStringParams();
+        params.put(DEFAULT_IMAGE_TAG, "File name of the picture to render for this brick.");
+        return params;
     }
 }
