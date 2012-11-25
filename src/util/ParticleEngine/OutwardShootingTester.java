8f1746433f1a0863f5b3c92b69999894d60876d0
Kathleen Lan <kathle.lan@gmail.com>
2012-11-25 15:44:15 -0500

    [ParticleEngine] Tester for outward-moving particle engine effect in multiple directions (currently given 4 directions); run the ParticleEngineTester class to view

---
+++ src/util/ParticleEngine/OutwardShootingTester.java
@@ -0,0 +1,46 @@
+package util.ParticleEngine;
+
+import java.awt.Image;
+import java.awt.Point;
+
+import javax.swing.ImageIcon;
+
+import util.calculator.VectorCalculator;
+
+/**
+ * Tests an outward-moving particle engine effect. There are 4 "directions of origin" - particles have one of the four following directions:
+ * (-1,-1), (1,1), (-1,1), (1,-1).
+ * 
+ * @author Kathleen
+ *
+ */
+public class OutwardShootingTester extends ParticleEngineTestingUnit{
+
+	private static Image particleImage;
+	private static Point position = new Point (300,300);
+	private static Point velocity = new Point(0,-1);
+	private static Point[] velocities = {new Point(-1,1), new Point(-1,-1), new Point(1,-1), new Point(1,1)};
+	private static int tolerance = 30;
+	private static int length = 1000;
+
+	private static int densityStartNum = 100;
+	private static int densityEndNum = 100001;
+	private static int densityStepCount = 10000;
+
+	private VectorCalculator vcalculator;
+	
+	@Override
+	protected void setUpParticleEngines() {
+		//these 2 lines of code are from David's ParticleTestApplet class
+		ImageIcon temp = new ImageIcon(
+				DensityTester.class.getResource("explosion.png"));
+		Image particleImage = temp.getImage();
+		
+		vcalculator = new VectorCalculator();
+		for (int j=0; j<velocities.length-3; j++){
+			addParticleEngine(densityStartNum, particleImage, position, vcalculator.scaleVelocity(10,velocities[j]), tolerance, length,0.0,1);
+		}
+
+	}
+
+}
