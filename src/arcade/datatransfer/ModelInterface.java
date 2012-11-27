4565cde75e95df38aa5b8402a54d42036e97b151
Michael Deng <michael.deng@duke.edu>
2012-11-27 07:02:28 -0500

    [arcade] added username delete on arcade exit

--- src/arcade/datatransfer/ModelInterface.java
+++ src/arcade/datatransfer/ModelInterface.java
@@ -24,14 +24,14 @@ public class ModelInterface {
         myArcade = a;
 
         // myGameCenter = new GameCenter(); // GameCenter is currently broken.
-        mySocialCenter = SocialCenter.getInstance();
+        // mySocialCenter = SocialCenter.getInstance();
+        
     }
 
     // #############################################################
     // Implemented by the Game team
     // #############################################################
 
-    
     public String getGameName () {
         // TODO Auto-generated method stub
         return null;
@@ -66,50 +66,45 @@ public class ModelInterface {
         // TODO Auto-generated method stub
         return null;
     }
-    
-    
-    
-    
-    
+
     // #############################################################
     // Implemented by the User team
     // #############################################################
 
-    
     /**
-     * Should change the user of UserLink to whatever user is successfully logged in.
+     * Should change the user of UserLink to whatever user is successfully
+     * logged in.
+     * 
      * @param username String of the username to be logged in.
-     * @param password Array of characters for the attempted password. Must be array.
+     * @param password Array of characters for the attempted password. Must be
+     *        array.
      * @return True if successful login, false if unsuccessful.
      */
-    public boolean loginUser(String username, char[] password){
-        // TODO Implement this. Return true if login successful, false if unsuccessful.
+    public boolean loginUser (String username, char[] password) {
+        // TODO Implement this. Return true if login successful, false if
+        // unsuccessful.
         return false;
     }
 
-    public String getUserName() {
+    public String getUserName () {
         // TODO Auto-generated method stub
         return null;
-        
+
     }
-    
-    public Image getUserProfilePicture() {
-        // TODO Auto-generated method stub
-        return null;
-    }
-    
-    public List<String> getListOfGamesPlayed() {
+
+    public Image getUserProfilePicture () {
         // TODO Auto-generated method stub
         return null;
     }
-    
-    public List<Integer> getListOfScores() {
+
+    public List<String> getListOfGamesPlayed () {
         // TODO Auto-generated method stub
         return null;
     }
-    
-    
-    
-    
-    
+
+    public List<Integer> getListOfScores () {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
 }
