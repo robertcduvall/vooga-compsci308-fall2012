82b901e241331b7295abe6f03d774e9c9d627130
Howard Chung <howard.chung@duke.edu>
2012-11-27 15:52:13 -0500

    modified hash function

--- src/util/encrypt/Hasher.java
+++ src/util/encrypt/Hasher.java
@@ -5,7 +5,7 @@ public class Hasher {
     public static String hashCode (String string) {
         Integer hash = 0;
         for (int i = 0; i < string.length(); i++) {
-            hash += (int) string.charAt(i);
+            hash += (int) string.charAt(i)*(i+31);
         }
         return hash.toString();
     }
