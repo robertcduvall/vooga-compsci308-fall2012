53ebbec1f1ffe337a89a67d4cdd843e1a2a344d6
Howard Chung <howard.chung@duke.edu>
2012-11-27 16:57:29 -0500

    uses default string hash function

--- src/util/encrypt/Hasher.java
+++ src/util/encrypt/Hasher.java
@@ -3,10 +3,15 @@ package util.encrypt;
 public class Hasher {
 
     public static String hashCode (String string) {
-        Integer hash = 0;
-        for (int i = 0; i < string.length(); i++) {
-            hash += (int) string.charAt(i)*(i+31);
-        }
+        Integer hash =string.hashCode();
         return hash.toString();
+    }
+    
+    public static String encrypt(String string){
+        return string;
+    }
+    
+    public static String decrypt(String string){
+        return string;
     }
 }
