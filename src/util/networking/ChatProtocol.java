241ddd88f4364c1877aeafae1bf4ef3b20627c40
Connor Gordon <connor.gordon@duke.edu>
2012-11-28 14:38:22 -0500

    [networking] renamed networking package and moved classes accordingly

--- src/util/networking/ChatProtocol.java
+++
@@ -1,7 +0,0 @@
-package util.networking;
-
-public interface ChatProtocol{
-        String sendMessage(String to, String dest, String body);
-        String queryRoomInformation(String roomName);
-        String openStream(String dest);    
-}
