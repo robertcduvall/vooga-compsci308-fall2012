325dfb5317cdeba37574bba0c45ce0d503d4c942
Oren Bukspan <obukspan@gmail.com>
2012-11-30 00:17:31 -0500

    [networking] restructured classes again. Possibly finally have a design #i<3networking

--- src/util/networking/chat/ChatService.java
+++ src/util/networking/chat/ChatService.java
@@ -6,6 +6,8 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.Socket;
+import java.util.Map;
+import util.networking.Server;
 import util.networking.Service;
 
 /**
@@ -15,21 +17,24 @@ import util.networking.Service;
 public class ChatService implements Service {
 
     private ChatProtocol myProtocol;
+    private ChatServer myServer;
+    private Map<String, Socket> myUsersToSockets;
 
     public ChatService(ChatProtocol cp) {
         myProtocol = cp;
     }
 
     @Override
-    public void serve (InputStream in, OutputStream out) throws IOException {
-
-        while (true) {
-            BufferedReader input = new BufferedReader(new InputStreamReader(in));
-            input.readLine();
-        }
-
+    public void serve (Socket socket, Server server) {
+        myServer = (ChatServer) server;
+        
+        
     }
 
+    /**
+     * Get the requested port for the protocol.
+     * @return Requested port for the protocol.
+     */
     public int getPort () {
         return myProtocol.getPort();
     }
