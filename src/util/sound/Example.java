02133e1667e0864306aeab91a54b3294f468acbf
vo.zavidovych@gmail.com <vo.zavidovych@gmail.com>
2012-12-02 02:34:09 -0500

    more robust sound util

--- src/util/sound/Example.java
+++ src/util/sound/Example.java
@@ -3,16 +3,20 @@ package util.sound;
 public class Example {
     public static void main (String[] args) {
         SoundPlayer p = new SoundPlayer("src/util/sound/sounds/teleport.wav");
+        // every time playOnce is called, a new thread is created. it would be removed after playback is over
         p.playOnce();
-        p = new SoundPlayer("src/util/sound/sounds/laser.wav");
-        p.startLoop();
+        p.playOnce();
+        SoundPlayer p1 = new SoundPlayer("src/util/sound/sounds/laser.wav");
+        // loop also runs in its own thread.
+        p1.startLoop();
         try {
             Thread.sleep(3000);
         }
         catch (InterruptedException e) {
         }
-        p.stopLoop();
-        p = new SoundPlayer("src/util/sound/sounds/benny_benassi.wav");
-        p.playOnce();
+        // loop needs to be stopped at some point
+        p1.stopLoop();
+        SoundPlayer p2 = new SoundPlayer("src/util/sound/sounds/benny_benassi.wav");
+        p2.playOnce();
     }
 }
