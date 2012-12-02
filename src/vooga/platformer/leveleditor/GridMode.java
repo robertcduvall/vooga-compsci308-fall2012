a81b4f9949f545d860a5413f3d524734f3a5c080
paul.dannenberg <google.paul.dannenberg@gmail.com>
2012-12-02 03:17:18 -0500

    added constructor to GridMode class that assigns a default grid size.

--- src/vooga/platformer/leveleditor/GridMode.java
+++ src/vooga/platformer/leveleditor/GridMode.java
@@ -24,39 +24,50 @@ public class GridMode extends PlacementMode {
      * screen that the grid is being displayed on.
      */
     private static final Dimension TILE_SIZE = new Dimension(50, 50);
+    private static final Dimension DEFAULT_GRID_SIZE = new Dimension(600, 400);
+
+    /**
+     * Creates a new object capable of managing selections of objects.
+     * The grid size is set to a default size of 600 x 400 pixels.
+     */
+    public GridMode () {
+        super();
+        myGrid = new Grid(new Point(), DEFAULT_GRID_SIZE, TILE_SIZE);
+    }
 
     /**
      * Creates an object capable of managing selections of objects.
      * 
      * @param size The size of the total grid.
      */
-    public GridMode(Dimension size) {
+    public GridMode (Dimension size) {
         super();
         myGrid = new Grid(new Point(), size, TILE_SIZE);
     }
 
     @Override
-    public void sendCursorPosition(int x, int y) {
+    public void sendCursorPosition (int x, int y) {
         super.sendCursorPosition(x, y);
         myGrid.highlightTile(x, y);
     }
 
     @Override
-    public void primaryButtonPress(int x, int y) {
+    public void primaryButtonPress (int x, int y) {
         if (getSelected() == null) {
             for (IEditorObject toSelect : getSelectables()) {
                 if (toSelect.contains(x, y) && !myGrid.containsTile(toSelect)) {
                     setSelected(toSelect);
                 }
             }
-        } else {
+        }
+        else {
             myGrid.position(getSelected());
             setSelected(null);
         }
     }
 
     @Override
-    public void paint(Graphics2D pen) {
+    public void paint (Graphics2D pen) {
         super.paint(pen);
         myGrid.paint(pen);
     }
