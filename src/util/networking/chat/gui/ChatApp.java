d8c96d79d8079a3a67f37179b2a7c80ce48bc787
Connor Gordon <connor.gordon@duke.edu>
2012-12-03 13:04:41 -0500

    [networking] integrating Client with GUI

--- src/util/networking/chat/gui/ChatApp.java
+++ src/util/networking/chat/gui/ChatApp.java
@@ -5,6 +5,7 @@ import java.awt.Dimension;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.io.IOException;
+import java.util.Arrays;
 import javax.swing.AbstractAction;
 import javax.swing.BorderFactory;
 import javax.swing.ButtonGroup;
@@ -32,11 +33,19 @@ public class ChatApp {
     private static JFrame frame; 
     
     public static void main (String[] args) {
-        String userName = login("");
-        frame = new JFrame("Greetings, " + userName +"! Chat. Connect. Play.");
+        try {
+            ChatClient c = new ChatClient("wl-10-190-55-34.wireless.duke.local", new GordonBukspanProtocol());
+            String userName = login("", c);
+            frame = new JFrame("Greetings, " + userName +"! Chat. Connect. Play.");
+        }
+        catch (IOException e) {
+            System.out.println("Cannot connect to server");
+        }
+        
+        
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         ExampleGUI eg;
-        eg = new ExampleGUI(/*new ChatClient("", new GordonBukspanProtocol()),*/ userName);
+        eg = new ExampleGUI(/*new ChatClient("", new GordonBukspanProtocol()),*/);
         frame.add(eg);
         JMenuBar menuBar = new JMenuBar();
         JMenu menu = new JMenu("File");
@@ -55,10 +64,10 @@ public class ChatApp {
 
     }
     
-    private static String login (String errorMessage) {
+    private static String login (String errorMessage, ChatClient c) {
         JPanel userPassPanel = new JPanel(new GridLayout(3,2));
         JLabel userLabel = new JLabel("Username (<12 char)");
-        JLabel passLabel = new JLabel("Password");
+        JLabel passLabel = new JLabel("Password (<5 char)");
         JTextField userTA = new JTextField(10);
         JPasswordField pass = new JPasswordField(10);
         userPassPanel.add(userLabel);
@@ -95,15 +104,25 @@ public class ChatApp {
         if(option == 0) // pressing OK button
         {
             String userName = userTA.getText().trim();
-            if(userName.length() > MAX_USER_CHAR)
-                return login("<html>Error: UserName entered is too long<br><br></html>");
-            else if(userName.length() == 0)
-                return login("<html>Error: A Zero Character UserName is NOT ALLOWED<br><br></html>");
             char[] password = pass.getPassword();
+            if(userName.length() > MAX_USER_CHAR)
+                return login("<html>Error: UserName entered is too long<br><br></html>", c);
+            else if(userName.length() == 0)
+                return login("<html>Error: A Zero Character UserName is NOT ALLOWED<br><br></html>", c);
+            else if (password.length < 5)
+                return login("<html>Error: Password must be at least 5 characters<br><br></html>",c);
+            if(login.isSelected()){
+                boolean status = c.loginWithTimeout(userName, new String(password), 5000);
+                if(!status) return login("<html>Error: Could not login. Please check your server connection.<br><br>", c);
+            }
+            else{
+                boolean status = c.registerWithTimeout(userName, new String(password), 5000);
+                if(!status) return login("<html>Error: Could not Properly Register. Please Try Again.<br><br>",c);
+            }
             return userName;
         }
         else if(option == 1)
-            return login("<html>Error: Please Enter a Username and Password. Otherwise you cannot chat.<br><br></html>");
+            return login("<html>Error: Please Enter a Username and Password. Otherwise you cannot chat.<br><br></html>", c);
         System.exit(0);
         return "";
     }
