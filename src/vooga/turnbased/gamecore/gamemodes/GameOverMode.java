984e109f443f6b41808bb355dde959f0a51474a4
Jenni Mercado <jdm45@duke.edu>
2012-12-02 23:58:38 -0500

    more checkstyling/javadocing YAY

--- src/vooga/turnbased/gamecore/gamemodes/GameOverMode.java
+++ src/vooga/turnbased/gamecore/gamemodes/GameOverMode.java
@@ -9,28 +9,33 @@ import java.util.List;
 import vooga.turnbased.gamecore.GameManager;
 import vooga.turnbased.gui.GameWindow;
 
+/**
+ * Mode designed to manage what happens when the game ends. 
+ * @author RPG team
+ *
+ */
 public class GameOverMode extends GameMode {
 
+    /**
+     * Constructor for GameOverMode.
+     * @param gm GameManager to which this mode belongs.
+     * @param modeName String name of mode.
+     * @param involvedIDs List of IDs of involved sprites.
+     */
     public GameOverMode (GameManager gm, String modeName, List<Integer> involvedIDs) {
         super(gm, modeName, involvedIDs);
     }
 
     @Override
     public void pause () {
-        // TODO Auto-generated method stub
-        
     }
 
     @Override
     public void resume () {
-        // TODO Auto-generated method stub
-        
     }
 
     @Override
     public void initialize () {
-        // TODO Auto-generated method stub
-        
     }
 
     @Override
@@ -42,8 +47,6 @@ public class GameOverMode extends GameMode {
 
     @Override
     public void update () {
-        // TODO Auto-generated method stub
-        
     }
 
     @Override
