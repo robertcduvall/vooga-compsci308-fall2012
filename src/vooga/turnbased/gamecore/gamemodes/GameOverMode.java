d6be13d2c527f40df0fbbef77654d04032b79911
dhowdyshell@gmail.com <dhowdyshell@gmail.com>
2012-12-03 00:13:24 -0500

    Merging in updated turnbased game

---
+++ src/vooga/turnbased/gamecore/gamemodes/GameOverMode.java
@@ -0,0 +1,58 @@
+package vooga.turnbased.gamecore.gamemodes;
+
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.Point;
+import java.util.List;
+
+import vooga.turnbased.gamecore.GameManager;
+import vooga.turnbased.gui.GameWindow;
+
+/**
+ * Mode designed to manage what happens when the game ends. 
+ * @author RPG team
+ *
+ */
+public class GameOverMode extends GameMode {
+
+    /**
+     * Constructor for GameOverMode.
+     * @param gm GameManager to which this mode belongs.
+     * @param modeName String name of mode.
+     * @param involvedIDs List of IDs of involved sprites.
+     */
+    public GameOverMode (GameManager gm, String modeName, List<Integer> involvedIDs) {
+        super(gm, modeName, involvedIDs);
+    }
+
+    @Override
+    public void pause () {
+    }
+
+    @Override
+    public void resume () {
+        playModeEntranceSound("GameOverSound");
+    }
+
+    @Override
+    public void initialize () {
+    }
+
+    @Override
+    public void paint (Graphics g) {
+        Image background = GameWindow.importImage("GameOverImage");
+        Dimension paneDim = getGameManager().getPaneDimension();
+        g.drawImage(background, 0, 0, paneDim.width, paneDim.width, null);
+    }
+
+    @Override
+    public void update () {
+    }
+
+    @Override
+    public void processMouseInput (int mousePressed, Point myMousePosition, int myMouseButton) {
+        
+    }
+
+}
---
+++ src/vooga/turnbased/gui/interactionpanel/StrategyOption.java
@@ -0,0 +1,91 @@
+package vooga.turnbased.gui.interactionpanel;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.FontMetrics;
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.Point;
+import java.awt.Rectangle;
+import java.awt.Toolkit;
+import java.awt.event.MouseEvent;
+import util.graphicprocessing.FontEffect;
+
+
+/**
+ * Options on the InteractionPanel used to display Strategies available
+ * 
+ * @author rex
+ * 
+ */
+public class StrategyOption {
+
+    private static final int FONT_SIZE = 16;
+    private static final int EPSILON = 10;
+    // Amount of change in x, y coordinates when highlighted
+    private static final Point DISPLACEMENT = new Point(2, 3);
+    private static final Color HIGHLIGHT_COLOR = Color.CYAN;
+    private static final Color DEFAULT_COLOR = Color.MAGENTA;
+    private String myOptionMessage;
+    private Point myPosition;
+    private FontMetrics myFontMetrics;
+    private Rectangle myRespondRegion;
+    private Color myColor;
+    private boolean isHighlighted;
+
+    public StrategyOption (String message, Point position) {
+        myOptionMessage = message;
+        myPosition = position;
+        myColor = DEFAULT_COLOR;
+        isHighlighted = false;
+    }
+
+    public void paintOption (Graphics g) {
+        Font optionFont = new Font("Helvetica", Font.BOLD, FONT_SIZE);
+        g.setFont(optionFont);
+        g.setColor(myColor);
+        FontEffect myGameFont = new FontEffect(g, optionFont);
+        myFontMetrics = g.getFontMetrics();
+        if (myRespondRegion == null) {
+            calculateRespondRegion();
+        }
+        Color topColor = new Color(0, 10, 115);
+        Color sideColor = new Color(60, 0, 115);
+        int layer = 4;
+        myGameFont.threeDimensionEffect(myOptionMessage, myColor, topColor, sideColor, layer,
+                                        myPosition);
+    }
+
+    private void calculateRespondRegion () {
+        int x = myPosition.x - EPSILON;
+        int y = myPosition.y - myFontMetrics.getHeight() - EPSILON;
+        int width = myFontMetrics.stringWidth(myOptionMessage) + 2 * EPSILON;
+        int height = myFontMetrics.getHeight() + 2 * EPSILON;
+        myRespondRegion = new Rectangle(x, y, width, height);
+    }
+
+    protected boolean highlight (Point mousePosition) {
+        if (myRespondRegion.contains(mousePosition)) {
+            myColor = HIGHLIGHT_COLOR;
+            isHighlighted = true;
+            myPosition.translate(DISPLACEMENT.x, DISPLACEMENT.y);
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    protected void dehighlight () {
+        if (isHighlighted) {
+            myColor = DEFAULT_COLOR;
+            isHighlighted = false;
+            myPosition.translate(-DISPLACEMENT.x, -DISPLACEMENT.y);
+        }
+    }
+
+    public boolean optionIsHighlighted () {
+        return isHighlighted;
+    }
+}
