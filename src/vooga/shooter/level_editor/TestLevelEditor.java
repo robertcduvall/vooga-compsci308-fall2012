8be40ef193446f6c2e8389109a6c07a79569c255
stephenalexbrowne <stephenalexbrowne@gmail.com>
2012-12-03 20:44:35 -0500

    Convert vooga.shooter.TestLevelEditor to a more robust JUnit test.

--- src/vooga/shooter/level_editor/TestLevelEditor.java
+++ src/vooga/shooter/level_editor/TestLevelEditor.java
@@ -3,40 +3,41 @@ package vooga.shooter.level_editor;
 import java.awt.Dimension;
 import java.awt.Point;
 import javax.xml.transform.TransformerException;
+import junit.framework.TestCase;
 import util.xml.XmlUtilities;
 import vooga.shooter.gameObjects.Enemy;
 
 
 /**
- * This is only a temporary test and should probably be
- * removed in the final project or replaced with a more
- * robust JUnit test.
+ * Tests the loadLevel and storeLevel functions
+ * of LevelFactory. The basic process is:
  * 
- * @author Alex
+ * 1) make a level
+ * 2) convert it to xml
+ * 3) use the xml to make a new level (a copy)
+ * 4) compare the new level (the copy) to the original
+ * 
+ * @author Alex Browne
  * 
  */
-public class TestLevelEditor {
+public class TestLevelEditor extends TestCase {
 
-    public static void main (String args[]) {
-        new TestLevelEditor();
-    }
+    public void testLevelStoreAndLoad () {
 
-    public TestLevelEditor () {
-        String project_path = System.getProperty("user.dir");
-        String image_path = project_path
-                + "/src/vooga/shooter/images/alien.png";
-        String file_path = project_path
-                + "/src/vooga/shooter/levels/level1.xml";
-        Level l = new Level(image_path);
+        // Instantiate a level class and add a single enemy to it...
+        String projectPath = System.getProperty("user.dir");
+        String imagePath = projectPath + "/src/vooga/shooter/images/alien.png";
 
+        Level level = new Level(imagePath);
         Enemy enemy = new Enemy(new Point(0, 0), new Dimension(10, 10),
-                new Dimension(100, 100), image_path, new Point(0, 0), 10);
-        
-        l.addSprite(enemy);
+                new Dimension(100, 100), imagePath, new Point(0, 0), 10);
+        level.addSprite(enemy);
 
+        // Convert the level to an xml string using LevelFactory
+        String originalString = null;
         try {
-            System.out.println(XmlUtilities.getXmlAsString(LevelFactory.storeLevel(l)));
-            System.out.println("done");
+            originalString = XmlUtilities.getXmlAsString(LevelFactory
+                    .storeLevel(level));
         }
         catch (TransformerException e) {
             // TODO Auto-generated catch block
@@ -44,16 +45,30 @@ public class TestLevelEditor {
             e.printStackTrace();
         }
 
-        Level newLevel = LevelFactory.loadLevel(LevelFactory.storeLevel(l));
+        // Make a copy of the level by loading the xml data in LevelFactory
+        Level newLevel = LevelFactory.loadLevel(LevelFactory.storeLevel(level));
 
+        // Convert the copy to an xml string
+        String newString = null;
         try {
-            System.out.println(XmlUtilities.getXmlAsString(LevelFactory.storeLevel(l)));
+            newString = XmlUtilities.getXmlAsString(LevelFactory
+                    .storeLevel(level));
         }
         catch (TransformerException e) {
             // TODO Auto-generated catch block
             System.err.println("ERROR: " + e.getMessage());
             e.printStackTrace();
         }
+
+        // make sure neither string is null
+        assertTrue("The original level string is not null",
+                originalString != null);
+        assertTrue("The new level string is not null", newString != null);
+
+        // if the two strings are identical, we can assume LevelFactory stored
+        // and loaded the level correctly.
+        assertEquals("The new level is an exact duplicate of the original",
+                originalString, newString);
 
     }
 
