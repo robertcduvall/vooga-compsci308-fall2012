100d5d34cbb45e7ba36e5b5693d06c52ba892c3a
Grant Oakley <geo4@gmail.com>
2012-12-04 00:03:27 -0500

    Made game instruction more readable.

--- src/games/platformerdemo/Main.java
+++ src/games/platformerdemo/Main.java
@@ -1,25 +1,27 @@
 package games.platformerdemo;
 
 import java.awt.Dimension;
-
 import javax.swing.JFrame;
 import vooga.platformer.core.PlatformerController;
 import vooga.platformer.core.inputinitializer.SimpleOnePlayerInputInitializer;
 
+
 /**
  * A class that creates a JFrame to hold the Game object
  * and launches the game.
+ * 
  * @author Niel
  * @author Yaqi
  */
 
 public class Main {
-    
+
     public static void main (String[] args) {
         JFrame frame = new JFrame("Demo Game");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        PlatformerController controller = new PlatformerController("src/vooga/platformer/data/NewLevel.xml",
-                new SimpleOnePlayerInputInitializer());
+        PlatformerController controller =
+                new PlatformerController("src/vooga/platformer/data/NewLevel.xml",
+                                         new SimpleOnePlayerInputInitializer());
 
         frame.getContentPane().add(controller);
         frame.pack();
@@ -29,12 +31,11 @@ public class Main {
 
     public static void paintString (PlatformerController controller) {
         Dimension canvasSize = controller.getSize();
-        controller.paintString("M - Menu", canvasSize.width * 3 / 5,canvasSize.height / 4);
-        controller.paintString("¡û ¡ú - Move left and right", canvasSize.width * 3 / 5,
-                canvasSize.height / 4 + 15);
-        controller.paintString("¡ü - Jump", canvasSize.width * 3 / 5,
-                canvasSize.height / 4 + 30);
+        controller.paintString("M - Menu", canvasSize.width * 3 / 5, canvasSize.height / 4);
+        controller.paintString("<- -> - Move left and right", canvasSize.width * 3 / 5,
+                               canvasSize.height / 4 + 15);
+        controller.paintString("UP - Jump", canvasSize.width * 3 / 5, canvasSize.height / 4 + 30);
         controller.paintString("Space - Shoot", canvasSize.width * 3 / 5,
-                canvasSize.height / 4 + 45);
+                               canvasSize.height / 4 + 45);
     }
 }
