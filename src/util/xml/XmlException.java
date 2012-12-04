fda81a3a3e783580c737eeffa73200bbc6bcb92e
dhowdyshell@gmail.com <dhowdyshell@gmail.com>
2012-12-03 23:53:09 -0500

    xml exception is mad at us :(

--- src/util/xml/XmlException.java
+++ src/util/xml/XmlException.java
@@ -3,15 +3,15 @@ package util.xml;
 /**
  * A general exception that represents all possible Java Reflection exceptions.
  * 
- * @author Robert C. Duvall
+ * @author Robert C. Duvall, fixed by Rex
  */
 @SuppressWarnings("serial")
-public final class XmlException extends RuntimeException {
-    public XmlException (String s) {
+public final class XMLException extends RuntimeException {
+    public XMLException (String s) {
         super(s);
     }
 
-    public XmlException (String s, Throwable cause) {
+    public XMLException (String s, Throwable cause) {
         super(s, cause);
     }
 }
