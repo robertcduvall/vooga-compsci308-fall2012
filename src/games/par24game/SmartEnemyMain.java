a13f31e03531d4ca7b245aa46657983b261b46f7
Patrick Royal <arealpanickytroll@gmail.com>
2012-12-07 18:33:28 -0500

    Created package for new game

---
+++ src/games/par24game/SmartEnemyMain.java
@@ -0,0 +1,11 @@
+package games.par24game;
+
+import vooga.shooter.gameplay.Game;
+
+public class SmartEnemyMain {
+
+    public static void main(String [] args) {
+        Game myGame = new Game();
+        myGame.runGame(null, null);
+    }
+}
