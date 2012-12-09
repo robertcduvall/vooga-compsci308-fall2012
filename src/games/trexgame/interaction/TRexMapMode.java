a67e964f842577a7caefa89c3b5547da34b071aa
Rex Ying <zy26@duke.edu>
2012-12-09 00:09:34 -0500

    add cheat keys

--- src/games/trexgame/interaction/TRexMapMode.java
+++ src/games/trexgame/interaction/TRexMapMode.java
@@ -21,6 +21,16 @@ public class TRexMapMode extends MapMode {
 		involvedIDs.add(getPlayer().getID());
 		flagCondition("viewstats", involvedIDs);
 	}
+	
+	/**
+	 * cheat key
+	 * for debugging purposes
+	 */
+	public void jumpToLevelTwo() {
+		List<Integer> involvedIDs = new ArrayList<Integer>();
+		involvedIDs.add(getPlayer().getID());
+		flagCondition("entermap2", involvedIDs);
+	}
 
 	@Override
 	public void configureInputHandling() {
@@ -28,6 +38,8 @@ public class TRexMapMode extends MapMode {
 		try {
 			getGameManager().getKeyboardController().setControl(KeyEvent.VK_A,
 					KeyboardController.PRESSED, this, "showStats");
+			getGameManager().getKeyboardController().setControl(KeyEvent.VK_J,
+					KeyboardController.PRESSED, this, "jumpToLevelTwo");
 		} catch (NoSuchMethodException e) {
 			e.printStackTrace();
 		} catch (IllegalAccessException e) {
