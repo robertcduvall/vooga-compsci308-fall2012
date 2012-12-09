2b2b127bd232a63bbb6e1f3aa3519038a0baf78c
Oren Bukspan <obukspan@gmail.com>
2012-12-08 22:50:41 -0500

    [networking] Updated NetworkedDataTable with protocol calls and created DataCommand

--- src/util/networking/datatable/NetworkedDataTable.java
+++ src/util/networking/datatable/NetworkedDataTable.java
@@ -2,6 +2,7 @@ package util.networking.datatable;
 
 import java.io.IOException;
 import java.util.Collection;
+import java.util.HashMap;
 import java.util.Map;
 import util.datatable.ModifiableRowElement;
 import util.datatable.UnmodifiableRowElement;
@@ -12,53 +13,84 @@ import util.networking.Client;
 
 public class NetworkedDataTable {
     
-    //Client -- Accessor Client
+    Client myClient;
     //Client -- Modifier Client
+    private DataProtocol myProtocol;
     
     public NetworkedDataTable (DataProtocol d, String host) throws IOException {
         
     }
 
-    public void addNewColumns (String strKey){}
+    public void addNewColumns (String strKey) {
+        String[] strArray = {strKey};
+        addNewColumns(strArray);
+       
+        
+    }
     
-    public void addNewColumns (String[] strArray){}
+    public void addNewColumns (String[] strArray){
+        myClient.send(myProtocol.createAddColumns(strArray));
+    }
     
-    public void addNewRow (Map<String , Object> mapEntry) {}
+    public void addNewRow (Map<String , Object> mapEntry) {
+        myClient.send(myProtocol.createAddRow(mapEntry));
+    }
     
-    public void addNewRow (ModifiableRowElement re){}
+    public void addNewRow (ModifiableRowElement re){
+        addNewRow(re.getAllEntries());
+    }
     
-    public void addNewRow (UnmodifiableRowElement re){}
+    public void addNewRow (UnmodifiableRowElement re){
+        addNewRow(re.getAllEntries());
+    }
     
-    public void deleteRowEntry (String strKey, Object value){} 
+    public void deleteRowEntry (String strKey, Object value){
+        myClient.send(myProtocol.createRowEntry(strKey, value));
+    } 
     
     public Collection<String> getColumnNames (){
+        myClient.send(myProtocol.createGetColumnNames());
         return null; 
     }
     
-    public void clear(){}
+    public void clear(){
+        myClient.send(myProtocol.createClear());
+    }
     
     public void editRowEntry (String strKeyRef, Object valueRef,
-                              String strKeyNew, Object valueNew){}
+                              String strKeyNew, Object valueNew){
+        Map<String, Object> map = new HashMap<String, Object>();
+        map.put(strKeyNew,  valueNew);
+        editRowEntry(strKeyRef, valueRef, map);
+    }
     
 
     public void editRowEntry (String strKeyRef, Object valueRef,
-            Map<String, Object> map) {}
+        Map<String, Object> map) {
+        myClient.send(myProtocol.createEdit(strKeyRef, valueRef, map));
+    }
     
    
     public Collection <UnmodifiableRowElement> getDataRows(){
+        myClient.send(myProtocol.createGetDataRows());
         return null;
     }
     
     
     
     public UnmodifiableRowElement find (String strKey, Object value){
+        myClient.send(myProtocol.createFind(strKey, value));
         return null;
     }
     
  
-    public void save (String location){}
+    public void save (String location){
+        myClient.send(myProtocol.createSave());
+    }
 
     public void load (String location) throws
-        RepeatedColumnNameException, InvalidXMLTagException{}
+        RepeatedColumnNameException, InvalidXMLTagException{
+        myClient.send(myProtocol.createLoad(location));
+    }
 
 }
