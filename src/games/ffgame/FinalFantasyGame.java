bc33d0fd2b8368bd89756884ce9d6de575581224
Mark Hoffman <mah75@duke.edu>
2012-12-11 11:16:25 -0500

    changes to ffgame

--- src/games/ffgame/FinalFantasyGame.java
+++ src/games/ffgame/FinalFantasyGame.java
@@ -1,7 +1,10 @@
 package games.ffgame;
 
 import java.awt.Image;
+import java.io.File;
+import java.util.ArrayList;
 import java.util.List;
+import javax.swing.ImageIcon;
 
 import vooga.turnbased.gui.GameWindow;
 import arcade.IArcadeGame;
@@ -13,24 +16,30 @@ import arcade.gamemanager.GameSaver;
  */
 public class FinalFantasyGame implements IArcadeGame {
 
-    private static final int WIDTH = 600;
-    private static final int HEIGHT = 450;
+    private static final int WIDTH = 800;
+    private static final int HEIGHT = 600;
     private static final String FILE_PATH = "src/games/ffgame/game.xml";
+    private static final File SCREENSHOT = new File(
+            "src/games/ffgame/arcade_images/ScreenShot.png");
+    private static final Image SCREENSHOT_IMG = new ImageIcon(SCREENSHOT.getAbsolutePath()).getImage();
 
     @Override
     public void runGame (String userPreferences, GameSaver s) {
         GameWindow myGameWindow = new GameWindow("FinalFantasy", "GameSetting", WIDTH, HEIGHT, FILE_PATH);
+        myGameWindow.changeActivePane(GameWindow.GAME);
 
     }
 
     @Override
     public List<Image> getScreenshots () {
-        return null;
+        List<Image> list = new ArrayList<Image>();
+        list.add(SCREENSHOT_IMG);
+        return list;
     }
 
     @Override
     public Image getMainImage () {
-        return null;
+        return SCREENSHOT_IMG;
     }
 
     @Override
@@ -41,5 +50,13 @@ public class FinalFantasyGame implements IArcadeGame {
     @Override
     public String getName () {
         return "Final Fantasy";
+    }
+
+    /**
+     * 
+     * @param args
+     */
+    public static void main (String[] args) {
+        new GameWindow("FinalFantasy", "GameSetting", WIDTH, HEIGHT, FILE_PATH);
     }
 }
